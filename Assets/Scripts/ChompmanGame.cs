fc8b19ef80ee8af01d6ca4b83d0d8b79083767e7
Invisible273 <invisible273@gmail.com>
2022-05-06 05:09:25 +0300

    constructed a proper level, added collectible pellets and score UI

--- Assets/Scripts/ChompmanGame.cs
+++ Assets/Scripts/ChompmanGame.cs
@@ -1,10 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class ChompmanGame : MonoBehaviour
 {
+    private const int MAX_SCORE_SIZE = 6;
+    private const int PELLET_VALUE = 100;
     [SerializeField]
     GameObject main_menu;
 
@@ -14,15 +18,27 @@ public class ChompmanGame : MonoBehaviour
     [SerializeField]
     GameObject chompman;
 
+    //[HideInInspector]
+    public static ChompmanGame instance = null;
+
     GameStates game_state;
-    PlayerController player_controller;
-    GameObject player;
     enum GameStates{
         MenuState,
         GameState
     }
 
+    PlayerController player_controller;
+    GameObject player;
     PlayerInput player_input;
+    int current_score = 0;
+    TextMeshProUGUI score_board = null;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
     void Start()
     {
         player_input = GetComponent<PlayerInput>();
@@ -53,17 +69,39 @@ public class ChompmanGame : MonoBehaviour
         if (game_state == GameStates.MenuState)
         {
             SwitchStateTo(GameStates.GameState);
+
             player = Instantiate(chompman, chompman.transform.position, Quaternion.identity);
             player.transform.parent = GameObject.Find("Characters").transform;
             player_controller = player.GetComponent<PlayerController>();
-            //Debug.Log("Spacebar!");
+
+            score_board = game_ui.GetComponentInChildren<TextMeshProUGUI>();
         }
     }
 
     void OnMove(InputValue movement_value)
     {
         Vector2 movement_vector = movement_value.Get<Vector2>();
-        //Debug.Log(movement_vector);
         player_controller.SetMovementVector(movement_vector);
     }
+
+    //Лучше реализовать через Event и Listener
+    public void ObjectTaken(string obj)
+    {
+        if (obj == "Pellet")
+            SetScore(current_score + PELLET_VALUE);
+    }
+
+    void SetScore(int score)
+    {
+        current_score = score;
+        if (score_board != null)
+        {
+            string score_string = current_score.ToString();
+            while (score_string.Length < MAX_SCORE_SIZE)
+                score_string = "0" + score_string;
+            score_board.text = "SCORE: " + score_string;
+        }
+    }
+
+    //public event Action<float> onScoreChange = delegate{Debug.LogWarning("WARNING: Score change amount not set!");};
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,64 +4,66 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField]
-    LayerMask collision_list;
+    public LayerMask collision_list;
     public float speed = 0;
-    public float grid_step = 0;
+    public float move_distance;
     private Rigidbody rb;
     private float radius;
-    private float movement_x;
-    private float movement_y;
     Vector3 destination = Vector3.zero;
     Vector3 movement_vector = Vector3.zero;
-    private float move_distance;
-    private float check_distance;
+    Vector2 last_movement_attempt = Vector2.zero;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         radius = GetComponent<SphereCollider>().radius * transform.localScale.x;
-        Debug.Log(radius);
         destination = transform.position;
-
-        move_distance = radius;
-        check_distance = move_distance + radius;
     }
 
     public void SetMovementVector(Vector2 movement)
     {
-        movement_x = movement.x;
-        movement_y = movement.y;
-        if (movement_x == 0 && movement_y == 0)
-             movement_vector = Vector3.zero;
-        else if (Mathf.Abs(movement_x) > Mathf.Abs(movement_y))
+        last_movement_attempt = movement;       
+        if (movement.x == 0 && movement.y == 0)
+            movement_vector = Vector3.zero;
+        else if (Mathf.Abs(movement.x) > Mathf.Abs(movement.y))
         {
-            if(movement_x > 0 && ValidateMovement(Vector3.right, check_distance))
+            if(movement.x > 0 && ValidateMovement(Vector3.right, move_distance))
                 movement_vector = move_distance * Vector3.right;
-            if(movement_x < 0 && ValidateMovement(Vector3.left, check_distance))
+            else if(movement.x < 0 && ValidateMovement(Vector3.left, move_distance))
                 movement_vector = move_distance * Vector3.left;
+            else
+                movement_vector = Vector3.zero;
         } 
         else
         {
-            if(movement_y > 0 && ValidateMovement(Vector3.forward, check_distance))
+            if(movement.y > 0 && ValidateMovement(Vector3.forward, move_distance))
                 movement_vector = move_distance * Vector3.forward;
-            if(movement_y < 0 && ValidateMovement(Vector3.back, check_distance))
+            else if(movement.y < 0 && ValidateMovement(Vector3.back, move_distance))
                 movement_vector = move_distance * Vector3.back;
+            else
+                movement_vector = Vector3.zero;
         }
     }
 
     void FixedUpdate()
     {
-        // Vector3 movement = new Vector3(movement_x, 0.0f, movement_y);
-        // rb.AddForce(movement * speed);
         Vector3 movement = Vector3.MoveTowards(transform.position, destination, speed);
         rb.MovePosition(movement);
 
-        if ((Vector3)transform.position == destination && ValidateMovement(Vector3.Normalize(movement_vector), check_distance)){
-            destination = (Vector3)transform.position + movement_vector;
-        }        
+        if ((Vector3)transform.position == destination)
+        {
+            if (movement_vector.magnitude > 0 && ValidateMovement(movement_vector, move_distance))
+            {
+                destination = (Vector3)transform.position + movement_vector;
+            }
+            else if (movement_vector.magnitude.Equals(0) && !last_movement_attempt.Equals(Vector2.zero))
+            {
+                SetMovementVector(last_movement_attempt);
+            }
+        }              
     }
 
-    bool ValidateMovement(Vector3 dir, float check_distance){
+    bool ValidateMovement(Vector3 dir, float mov_dist){
+        float check_distance = mov_dist + radius;
         Vector3 pos = transform.position;
         Vector3 central = Vector3.Normalize(dir)*(check_distance);
         Vector3 right = Vector3.Cross(Vector3.up,Vector3.Normalize(dir))*radius;
@@ -71,8 +73,16 @@ public class PlayerController : MonoBehaviour
         bool hit_left = Physics.Linecast(pos, pos + central + left, collision_list);
         Debug.DrawLine(pos,pos + central + right, Color.red, 0.1f);
         Debug.DrawLine(pos,pos + central + left, Color.red, 0.1f);
-        Debug.Log(!(hit_right || hit_left));
         return !(hit_right || hit_left);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Pellet"))
+        {
+            ChompmanGame.instance.ObjectTaken(other.gameObject.tag);
+            other.gameObject.SetActive(false);
+        }     
     }
 }
  
